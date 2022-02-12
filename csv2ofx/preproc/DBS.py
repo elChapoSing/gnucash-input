deea8b4f76a32307ac0379dce27f861f7be5c3a2
elChapoSing <github@pochat.eu>
2022-02-12 14:13:49 +0800

    change way to get description and notes

--- csv2ofx/preproc/DBS.py
+++ csv2ofx/preproc/DBS.py
@@ -12,21 +12,29 @@ def preprocess(folder, filename):
     data = [line.rstrip("\n") for line in data]
     for i in range(0, len(data)):
         print(data[i])
-        if any(ext in data[i] for ext in ["AWL","POS"]):
-            data[i] = data[i][:-1] # take off end comma for AWL transactions
-        data[i] = data[i].replace(", ,",",,")
-        data[i] = re.sub(r", +",",",data[i])
+        if any(ext in data[i] for ext in ["AWL", "POS"]):
+            data[i] = data[i][:-1]  # take off end comma for AWL transactions
+        data[i] = data[i].replace(", ,", ",,")
+        data[i] = re.sub(r", +", ",", data[i])
     with open(new_path, "w") as myfile:
         for line in data:
-            myfile.write(line+"\n")
+            myfile.write(line + "\n")
     myfile.close()
 
     a = pd.read_csv(new_path, skiprows=17, skip_blank_lines=True, error_bad_lines=False, index_col=False)
     a["Transaction Date"] = pd.to_datetime(a["Transaction Date"], format="%d %b %Y").dt.strftime("%Y-%m-%d")
     a["Amount"] = np.where(a["Debit Amount"].isna(), a["Credit Amount"], -a["Debit Amount"])
-    a["Description"] = np.where(a["Transaction Ref1"].isna(), "", a["Transaction Ref1"])
-    a["Notes"] = np.where(a["Transaction Ref2"].isna(), "", a["Transaction Ref2"]) + \
-                 np.where(a["Transaction Ref3"].isna(), "", a["Transaction Ref3"])
+
+    ref1 = np.where(a["Transaction Ref1"].isna(), "", a["Transaction Ref1"])
+    ref2 = np.where(a["Transaction Ref2"].isna(), "", a["Transaction Ref2"])
+    ref3 = np.where(a["Transaction Ref3"].isna(), "", a["Transaction Ref3"])
+
+    a["Description"] = ref1
+    a["Description"] = np.where(a["Description"] != "", a["Description"], ref2)
+    a["Description"] = np.where(a["Description"] != "", a["Description"], ref3)
+
+
+    a["Notes"] = np.where(a["Transaction Ref1"] == a["Description"], ref2+ref3, ref3)
     a = a[["Transaction Date", "Amount", "Description", "Notes"]]
     a.to_csv(new_path, index=False)
     return new_path
